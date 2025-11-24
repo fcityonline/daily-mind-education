156170f86afb5e732183adbdcbd88725db7a7d8a
www.fcity.online <contact.fcity.online@gmail.com>
2025-11-25 03:27:15 +0530

    Delete frontend directory

--- frontend/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-// frontend/src/components/Header.jsx
-export default function Header() {
-  return (
-    <header className="header">
-      <h1 className="logo">DailyQuiz</h1>
-    </header>
-  );
-}
--- frontend/src/socket.js
+++
@@ -1,7 +0,0 @@
-import { io } from "socket.io-client";
-
-const token = localStorage.getItem("token");
-
-export const socket = io(process.env.REACT_APP_API_URL.replace("/api", ""), {
-  auth: { token }
-});
